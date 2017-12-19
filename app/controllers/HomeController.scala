1912a7d816075a6a5e4e09e7d120ffb0b0207344
Denys Kornilov <denys.kornilov@cargomedia.ch>
2017-12-19 01:29:27 +0100

    basic controllers

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,6 +7,9 @@ import play.api.mvc._
 import services._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+case class CompareResult(gt: Boolean)
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -26,17 +29,44 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index())
   }
 
-  def getPics = Action.async { implicit request =>
+  def getPics: Action[AnyContent] = Action.async { implicit request =>
     PictureService.getAllPictures.map { pic =>
       Ok(Json.toJson(pic))
     }
   }
 
-  def compare() = Action.async {
+  def compare(): Action[AnyContent] = Action.async {
     for {
       (ansA, ansB) <- CompareService.compareTwo
     } yield Ok(views.html.compare(ansA, ansB))
 
+  }
+
+  implicit val pictureReads: Reads[CompareResult] = Json.reads[CompareResult]
+  implicit val pictureWrites: OWrites[CompareResult] = Json.writes[CompareResult]
+
+  def compareResult(): Action[JsValue] = Action.async(parse.json) { request =>
+
+    println(request.body)
+
+    def compareResultResponseString(x: CompareResult): _root_.play.api.libs.json.JsString = {
+      if (x.gt) {
+        JsString("You selected left picture")
+      } else {
+        JsString("You selected right picture")
+      }
+    }
+
+    val x = request.body.validate[CompareResult].fold(
+      errors => {
+        BadRequest(errors.toString())
+      },
+      gt => {
+        Ok(compareResultResponseString(gt))
+      }
+    )
+
+    Future.successful(x)
   }
 
 }
