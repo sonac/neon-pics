e52633aa27cff56dd8c93e9f576b0a2b7f5f6d90
Denys Kornilov <denys.kornilov@cargomedia.ch>
2017-12-26 21:17:57 +0100

    accept questionnaire answers via POST  /comparison-answer/

--- app/Module.scala
+++ app/Module.scala
@@ -3,24 +3,15 @@ import javax.inject._
 import com.google.inject.AbstractModule
 import net.codingwell.scalaguice.ScalaModule
 import play.api.{Configuration, Environment}
-import services.comparison.{QuestionnaireRepository, QuestionnaireRepositoryImpl}
+import services.comparison.{QuestionnaireAnswerRepository, QuestionnaireRepository, QuestionnaireRepositoryImpl}
 import services.{PostgresService, PostgresServiceProduction}
-import slick.jdbc.PostgresProfile
 
-class Module (environment: Environment, configuration: Configuration)
+class Module(environment: Environment, configuration: Configuration)
   extends AbstractModule
     with ScalaModule {
   override def configure(): Unit = {
-
-//    bind(classOf[PostgresService]).to(classOf[PostgresServiceProduction])
     bind[PostgresService].to[PostgresServiceProduction]
     bind[QuestionnaireRepository].to[QuestionnaireRepositoryImpl].in[Singleton]
-//    bind(classOf[PostgresService]).to(classOf[PostgresServiceProduction])
-    // Use the system clock as the default implementation of Clock
-//    bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    // Ask Guice to create an instance of ApplicationTimer when the
-    // application starts.
-//    bind(classOf[ApplicationTimer]).asEagerSingleton()
-//    bind(classOf[Counter]).to(classOf[AtomicCounter])
+    bind[QuestionnaireAnswerRepository].to[QuestionnaireRepositoryImpl].in[Singleton]
   }
 }
