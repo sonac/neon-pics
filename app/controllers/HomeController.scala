694a34292f9f7d5dc314e414cf2b1b1937e39ced
Denys Kornilov <denys.kornilov@cargomedia.ch>
2017-12-26 19:10:11 +0100

    Removed initial unused implementation and default files

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,6 @@ import play.api.mvc._
 import services._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 case class CompareResult(gt: Boolean)
 
@@ -18,55 +17,12 @@ case class CompareResult(gt: Boolean)
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+  implicit val pictureReads: Reads[CompareResult] = Json.reads[CompareResult]
+  implicit val pictureWrites: OWrites[CompareResult] = Json.writes[CompareResult]
 
   def getPics: Action[AnyContent] = Action.async { implicit request =>
     PictureService.getAllPictures.map { pic =>
       Ok(Json.toJson(pic))
     }
   }
-
-  def compare(): Action[AnyContent] = Action.async {
-    for {
-      (ansA, ansB) <- CompareService.compareTwo
-    } yield Ok(views.html.compare(ansA, ansB))
-
-  }
-
-  implicit val pictureReads: Reads[CompareResult] = Json.reads[CompareResult]
-  implicit val pictureWrites: OWrites[CompareResult] = Json.writes[CompareResult]
-
-  def compareResult(): Action[JsValue] = Action.async(parse.json) { request =>
-
-    println(request.body)
-
-    def compareResultResponseString(x: CompareResult): _root_.play.api.libs.json.JsString = {
-      if (x.gt) {
-        JsString("You selected left picture")
-      } else {
-        JsString("You selected right picture")
-      }
-    }
-
-    val x = request.body.validate[CompareResult].fold(
-      errors => {
-        BadRequest(errors.toString())
-      },
-      gt => {
-        Ok(compareResultResponseString(gt))
-      }
-    )
-
-    Future.successful(x)
-  }
-
 }
